1de4a9f767e380feee90a32d86b49e14de3ce02d
alex <skrypko.aleksandr@gmail.com>
2019-07-29 14:30:05 +0300

    Refactored GildedRose update_quality method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,51 +3,44 @@ class GildedRose
     @items = items
   end
 
+  def update_item(item)
+    return if item.name == 'Sulfuras, Hand of Ragnaros'
+    item.sell_in -= 1
+    inc = case item.name
+          when 'Aged Brie'
+            1
+          when 'Sulfuras, Hand of Ragnaros'
+            0
+          when 'Backstage passes to a TAFKAL80ETC concert'
+            case
+            when item.sell_in > 10
+              1
+            when item.sell_in.between?(6, 10)
+              2
+            when item.sell_in.between?(1, 5)
+              3
+            else
+              -Float::INFINITY
+            end
+          when 'Conjured Mana Cake'
+            -2
+          else
+            -1
+          end
+    value = item.quality + inc
+    item.quality = case
+                   when value.negative?
+                     0
+                   when value > 50
+                     50
+                   else
+                     value
+                   end
+  end
+
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_item(item)
     end
   end
 end
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -2,10 +2,32 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+    it 'update quality' do
+      items = [
+          Item.new(name='+5 Dexterity Vest', sell_in=10, quality=20),
+          Item.new(name='Aged Brie', sell_in=2, quality=0),
+          Item.new(name='Elixir of the Mongoose', sell_in=5, quality=7),
+          Item.new(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=80),
+          Item.new(name='Sulfuras, Hand of Ragnaros', sell_in=-1, quality=80),
+          Item.new(name='Backstage passes to a TAFKAL80ETC concert', sell_in=15, quality=20),
+          Item.new(name='Backstage passes to a TAFKAL80ETC concert', sell_in=10, quality=49),
+          Item.new(name='Backstage passes to a TAFKAL80ETC concert', sell_in=5, quality=49),
+          Item.new(name='Conjured Mana Cake', sell_in=3, quality=6),
+      ]
+
+      items_day_1 = [
+          Item.new(name='+5 Dexterity Vest', sell_in=9, quality=19),
+          Item.new(name='Aged Brie', sell_in=1, quality=1),
+          Item.new(name='Elixir of the Mongoose', sell_in=4, quality=6),
+          Item.new(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=80),
+          Item.new(name='Sulfuras, Hand of Ragnaros', sell_in=-1, quality=80),
+          Item.new(name='Backstage passes to a TAFKAL80ETC concert', sell_in=14, quality=21),
+          Item.new(name='Backstage passes to a TAFKAL80ETC concert', sell_in=9, quality=50),
+          Item.new(name='Backstage passes to a TAFKAL80ETC concert', sell_in=4, quality=50),
+          Item.new(name='Conjured Mana Cake', sell_in=2, quality=4),
+      ]
+      GildedRose.new(items).update_quality
+      expect(items.map(&:to_s)).to eq items_day_1.map(&:to_s)
     end
   end
 end
